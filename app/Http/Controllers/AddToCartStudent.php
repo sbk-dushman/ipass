ee5ede7042281d91fff8005115a0130f8d51f106
Samat099 <79961944+Samat099@users.noreply.github.com>
2021-03-10 14:29:06 +0500

    Группы
    Кнопка добавить добавляет студента в корзину если уже студент в корзине он его не добавит

---
+++ app/Http/Controllers/AddToCartStudent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AddToCartStudent extends Controller
+{
+    public function addCart(Request $request)
+    {
+        
+    }
+}
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\CardStudent;
 use Illuminate\Http\Request;
 use App\Group;
 use App\ListStudent;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller
 {
@@ -14,16 +16,56 @@ class MainController extends Controller
         // dd($paginator);
         return view('home', compact('groups'));
     }
+
+    public function addCart($groupname, Request $request)
+    {
+        $data = $request->add_to_cart;
+        $StudName = ListStudent::where('id', $data)
+                                ->value('name');
+        $StudSurname = ListStudent::where('id', $data)
+                                ->value('surname');
+        $StudLastname = ListStudent::where('id', $data)
+                                ->value('lastname');
+        $StudGroup = ListStudent::where('id', $data)
+                                ->value('group');
+        $issetName = CardStudent::where([
+            'name' => $StudName,
+            'surname' => $StudSurname,
+            'lastname' => $StudLastname,
+            'group' => $StudGroup
+        ])->value('id');
+        if( $issetName == true ) {
+            return redirect()->back();
+        }else {
+            DB::table('card_students')->insert([
+                'name' => $StudName,
+                'surname' => $StudSurname,
+                'lastname' => $StudLastname,
+                'group' => $StudGroup
+            ]);
+        }
+        return redirect()->back();
+    }
+
     public function group($groupname)
     {
 
         $students = ListStudent::where('group', $groupname)->get();
+        // $emptyCart = CardStudent::where([
+        //     'name' => $this->StudName,
+        //     'surname' => $this->StudSurname,
+        //     'lastname' => $this->StudLastname,
+        //     'group' => $this->StudGroup
+        // ])->get('id');
         return view('group', compact('students'));
     }
-    public function selected(){
 
-        return view('selected');
+    public function selected()
+    {
+        $studentDatas = CardStudent::get();
+        return view('selected', compact('studentDatas'));
     }
+
     public function dropFile($var = null)
     {
         return view('dropFile');
