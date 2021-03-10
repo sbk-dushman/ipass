c792573fd2fa6527a15b989750e7061e3480b2e0
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2021-03-10 17:21:15 +0500

    решение конфликта
    1

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ Route::get('/','MainController@home')->name('home-URL');
 Route::get('/group{groupname?}','MainController@group')->name('group-URL');
 Route::post('/group{groupname?}','MainController@addCart')->name('group-URL');
 Route::get('/selected','MainController@selected')->name('selected-URL');
-// Дроп файлов
-Route::get('file-upload', 'FileUploadController@fileUpload')->name('file.upload');
-Route::post('file-upload', 'FileUploadController@fileUploadPost')->name('file.upload.post');
-// Route::get('/drop-file','MainController@dropFile')->name('drop-file');
+Route::get('/drop-file','MainController@dropFile')->name('drop-file');
 
 
 
