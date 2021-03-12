5f9bcff729d799dfac52ff8ff62758d4fd07bf74
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2021-03-12 11:40:12 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -50,14 +50,10 @@ class MainController extends Controller
 
         $students = ListStudent::where('group', $groupname)->get();
         $cartStudents = CardStudent::get();
-<<<<<<< HEAD
+
 
         //dump($cartStudents);
-=======
-        // dump($cartStudents);
 
-
->>>>>>> 78aceff898a837bd9dce07dca2e560a1fb2b9f5e
         return view('group', compact('students', 'cartStudents'));
     }
 
