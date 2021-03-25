e88a36525323de19877155fdef13c00986c4c02a
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2021-03-25 13:42:02 +0500

    новый дизайн переделка

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,11 +8,30 @@
     <link rel="stylesheet" href="/css/all.css">
 </head>
 <body>
-    <div class="container-main">
+    <main class="grid-container">
+        @section('sidebar')
+            <aside class="sidebar">
+        <div class="container">
+        <ul class="link-list">
+            <li class="link-list__item"><a href="#">1</a></li>
+            <li class="link-list__item"><a href="#">2</a></li>
+            <li class="link-list__item"><a href="#">3</a></li>
+            <li class="link-list__item"><a  href="#">4</a></li>
+            <li class="link-list__item"><a href="#">5</a></li>
+        </ul>
+
+        </div>
+      </aside>
+        @show
+        <div class="content">
+            @yield('content')
+        </div>
+    </main>
+    {{-- <div class="container-main">
 
     @yield('content')
 
-    </div>
+    </div> --}}
     <script
     src="https://code.jquery.com/jquery-3.6.0.min.js"
       integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,10 @@ const mix = require('laravel-mix');
 mix.styles([
     'resources/css/style.css',
     'resources/css/m_style.css',
+    'resources/css/all.css',
 ], 'public/css/all.css');
 mix.js(['resources/js/app.js',
-        'resources/js/jquery-3.6.0.js'
+        'resources/js/jquery-3.6.0.js',
+         'resources/js/all.js'
     ],'public/js/app.js');
 //     .sass('resources/sass/app.scss', 'public/css');
